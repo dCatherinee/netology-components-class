2fe321c222665ff7476d2e1737ae8ca244b19fb1
Kate Guseva <Marzia240015@gmail.com>
2023-01-17 18:22:42 +0300

    fixed: удаление конструктора в классовом компоненте

--- src/components/ShopItemClass/ShopItemClass.js
+++ src/components/ShopItemClass/ShopItemClass.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class ShopItemClass extends Component {
-  constructor(props){
-    super(props);
-  }
-
   render() {
     return (
       <div className="main-content">
